4909d1a63ea9e95d40c8bd127864530f8d3da4a8
NikMI9324 <nikitinmiron24@gmail.com>
2026-01-28 01:28:04 +0300

    Add main entities

---
+++ RssFeederBackend/RssFeeder.Domain/Entities/RssChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.ServerSentEvents;
+using System.Text;
+
+namespace RssFeeder.Domain.Entities
+{
+    public class RssChannel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<RssItem> Items { get; set; } = new();
+    }
+}
